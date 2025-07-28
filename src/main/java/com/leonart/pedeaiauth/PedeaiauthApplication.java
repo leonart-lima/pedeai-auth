992efd9de03e7286135c55f6c6a8df3476c1c4d6
leonart-lima <leonart.lima@protonmail.com>
2025-07-28 15:34:14 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/leonart/pedeaiauth/PedeaiauthApplication.java
@@ -0,0 +1,13 @@
+package com.leonart.pedeaiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedeaiauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedeaiauthApplication.class, args);
+	}
+
+}
