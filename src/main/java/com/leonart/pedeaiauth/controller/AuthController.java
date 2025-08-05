5c883788af6408d990d43760d2e9bc1ccc3af41b
leonart-lima <leonart.lima@protonmail.com>
2025-08-04 23:50:25 -0300

    Spring security

--- src/main/java/com/leonart/pedeaiauth/controller/AuthController.java
+++ src/main/java/com/leonart/pedeaiauth/controller/AuthController.java
@@ -2,12 +2,19 @@ package com.leonart.pedeaiauth.controller;
 
 import com.leonart.pedeaiauth.api.AuthApi;
 import com.leonart.pedeaiauth.dto.LoginRequestDTO;
+import com.leonart.pedeaiauth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AuthController implements AuthApi {
+
+    @Autowired
+    AuthService authService;
 
     @Override
     public ResponseEntity<String> authLoginPost(LoginRequestDTO body) {
-        return AuthApi.super.authLoginPost(body);
+        return ResponseEntity.ok(authService.authenticate(body.getUsername(), body.getPassword()));
     }
 }
